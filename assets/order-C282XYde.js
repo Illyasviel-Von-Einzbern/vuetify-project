99469ac01c8cf8fc2f08a2ed5ed672116872bb83
Illyasviel-Von-Einzbern <43869793+Illyasviel-Von-Einzbern@users.noreply.github.com>
2025-07-17 01:21:15 +0000

    Deploying to gh-pages from @ Illyasviel-Von-Einzbern/7_17front@a1de21dbeea513fd8c549b779470f7c05edb15d3 🚀

---
+++ assets/order-C282XYde.js
@@ -0,0 +1,1 @@
+import{bJ as r}from"./index-De5_2SVk.js";const t={create(){return r.apiAuth.post("/order")},getMy(){return r.apiAuth.get("/order/my")},getAll(){return r.apiAuth.get("/order/all")}};export{t as o};
